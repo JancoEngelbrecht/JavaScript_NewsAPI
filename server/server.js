3dce32e9d1c8e8bd3db3e1f059443ce541bb8bd0
Janco Engelbrecht <75523652+JancoEngelbrecht@users.noreply.github.com>
2024-09-07 11:34:11 +0200

    Update server.js
    Add GET for news from NewsAPI API.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const axios = require('axios');
 
 // Use the port assigned by Azure, or default to 3000 locally
 const port = process.env.PORT || 3000;
@@ -14,6 +15,21 @@ app.use(express.json());
 // Route to get home
 app.get('/', (req, res) => {
   res.json("janco");
+});
+
+//Get news from NewsAPI
+app.get('/news', async (req, res) => {
+  try {
+    const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+      params: {
+        country: 'us',
+        apiKey: 'a5c9c48cee5b4d81b4492fa66bf61661'
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching news data' });
+  }
 });
 
 // Route to get weather data
